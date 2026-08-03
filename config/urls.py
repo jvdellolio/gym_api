2f6e8e2b6fdcc846bcb00253898b313efe1a0e08
Joao Vitor <luhdens@gmail.com>
2026-08-03 16:57:36 -0300

    feat: mapeando endpoints do swagger e exercise

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,16 @@
 
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 from config.api import views
 
 urlpatterns = [
     path("exercise/", views.ExerciseList.as_view()),
+    path("exercise/<int:pk>", views.ExerciseDetail.as_view()),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
